a702539cfead39207934bef1f272e22d63bd28cc
rmercker <merckerrobert@yahoo.com>
2018-03-05 16:31:10 -0500

    Added doors open and navMesh
    Teh doors open when corridor path generated.
    navMesh now available on all levels.

---
+++ Assets/Scripts/Dungeon/DoorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+	void OnCollisionEnter (Collision c) {
+		Debug.Log ("I was Called" + c.gameObject.tag);
+		if (c.gameObject.tag.Equals ("Corridor")) {
+			gameObject.SetActive (false);
+		}
+	}
+}
