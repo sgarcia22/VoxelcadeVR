cd20ed8eaefe94e95d4e019391b3c1b469afc142
rmercker <merckerrobert@yahoo.com>
2018-03-12 20:53:49 -0400

    changes

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	[SerializeField]
+	private int health;
+	private int level;
+
+	void Start () {
+		level = 0;
+	}
+
+	public void updateHealth (int amount) {
+		health += amount;
+
+		if (health <= 0) {
+			//cause game reset
+		}
+	}
+
+	public int getHealth () {
+		return health;
+	}
+
+	public void gainLevel () {
+		++level;
+	}
+
+	public int getLevel () {
+		return level;
+	}
+}
