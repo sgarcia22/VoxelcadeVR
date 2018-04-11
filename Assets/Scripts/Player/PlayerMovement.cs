5014c00c50a217a47a080ed210b13e8b1cd873c5
Samantha Garcia <sgarcia22@ufl.edu>
2018-04-10 20:15:34 -0400

    Added Game Over

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour {
 	private bool pressMap = false;
 	private float gravity = 9.8f;
 	private Vector3 tempRigid;
+	public GameObject gameOver;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -35,27 +36,34 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 		//tempRigid.y -= gravity * Time.deltaTime;
 		//player.transform.position += tempRigid * Time.deltaTime;
-
-		if (currentTime >= timeTillPress)
-			pressMap = false;
-		//Movement
-		if (OVRInput.Get (OVRInput.Button.Two)) {
-			rb.MovePosition(player.transform.position + camera.transform.forward * Time.deltaTime * speed);
+		if (player.GetComponent<PlayerStats> ().getHealth () <= 0) {
+			if (!gameOver.activeSelf)
+				gameOver.SetActive (true);
+			if (OVRInput.Get (OVRInput.Button.One) || OVRInput.Get (OVRInput.Button.Two) || OVRInput.Get (OVRInput.Button.Three) || OVRInput.Get (OVRInput.Button.Four))
+				SceneManager.LoadScene (0);
 		}
-		if (OVRInput.Get (OVRInput.Button.One)) {
-			rb.MovePosition(player.transform.position - camera.transform.forward * Time.deltaTime * speed / 2);
+		else {
+			if (currentTime >= timeTillPress)
+				pressMap = false;
+			//Movement
+			if (OVRInput.Get (OVRInput.Button.Two)) {
+				rb.MovePosition(player.transform.position + camera.transform.forward * Time.deltaTime * speed);
+			}
+			if (OVRInput.Get (OVRInput.Button.One)) {
+				rb.MovePosition(player.transform.position - camera.transform.forward * Time.deltaTime * speed / 2);
+			}
+			if (OVRInput.Get (OVRInput.Button.Start)) {
+				SceneManager.LoadScene (1);
+			}
+			if (OVRInput.Get (OVRInput.Button.Four) && !pressMap) {
+				if (map.activeSelf == false)
+					map.SetActive (true);
+				else
+					map.SetActive (false);
+				pressMap = true;
+				currentTime = 0f;
+			}
+			currentTime += Time.deltaTime;
 		}
-		if (OVRInput.Get (OVRInput.Button.Start)) {
-			SceneManager.LoadScene (1);
-		}
-		if (OVRInput.Get (OVRInput.Button.Four) && !pressMap) {
-			if (map.activeSelf == false)
-				map.SetActive (true);
-			else
-				map.SetActive (false);
-			pressMap = true;
-			currentTime = 0f;
-		}
-		currentTime += Time.deltaTime;
 	}
 }
